18733320e943dc21576c74d5af776b5a34d9d05b
sai dutt <sai dutt>
2016-07-17 12:25:54 +0530

    making universe problem more efficient

--- spoj_problems/universeproblem.c
+++ spoj_problems/universeproblem.c
@@ -2,17 +2,20 @@
 
 int main()
 {
-	const long int index=1000000;
- 	long int no,array[index],i=0;
+	//const long int index=1000000;
+ 	//long int no,array[index],i=0;
+	long int no;
 
  	while(1)
  	{
  		scanf("%ld",&no);
  		if (no==42) break;
- 		array[i]=no;
- 		i++;
+		printf("\n%ld",no);
+ 		//array[i]=no;
+ 		//i++;
  	}
- 	for(no=0;no<i;no++)
+ 	/* for(no=0;no<i;no++)
  			printf("\n%ld",array[no]);
+	*/
  	return 0;
 }
