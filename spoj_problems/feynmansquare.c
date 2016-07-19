60f9f0cda1db01c03766a182e2ea52ea1757c8cf
sai dutt <sai dutt>
2016-07-19 13:16:51 +0530

    feynman problem (wrong answer)

---
+++ spoj_problems/feynmansquare.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+long int noofsqrs(int);
+
+int main()
+{
+  int no;
+  while(scanf("%d",&no))
+  {
+    if(no==0)
+     break;
+    printf("%ld\n",noofsqrs(no));
+  }
+  return 0;
+}
+
+long int noofsqrs(int n)
+{
+  long int sum=0;
+  if(n==1)
+    return 1;
+  sum+=n*n+noofsqrs(n-1);
+}
