e1d830d585be1668da3f8949741cc04defdd925d
sai dutt <sai dutt>
2016-09-09 17:12:56 +0530

    changed lcm-sum - TLE solution

--- spoj_problems/lcmsum.cpp
+++ spoj_problems/lcmsum.cpp
@@ -1,30 +1,54 @@
-//getting time limit exceeded error inspite of the solution being correct
+//TLE 
+//refered link : http://abhisharlives.blogspot.in/2012/06/really-fast-io-methods-for-programming.html 
+//Alternate more mathematical solution : http://blog.forthright48.com/2015/08/spoj-lcmsum-lcm-sum.html
+
 #include<stdio.h>
+#include<cstdio>
+#define gc getchar_unlocked     //fast-io : faster input compared to scanf
 //using namespace std;
 
-long gcd(long i,long n);
+//int gcd(int i,int n);
+/*void scanint(int &x)        //***to be used only online judges as "getchar_unlocked is thread unsafe as it locks the screen while getting the input" ***
+{
+  register int c=gc();
+  x=0; 
+  int neg=0; 
+  for(;(c<48 || c>57);c=gc());        //48= ascii for '0' ad 57 ascii for '9'
+  if(c=='-'){neg=1;c=gc();}
+  for(;c>48 && c<57;c=gc()){x=x*10+(c-48);}
+} 
+*/
+
 int main()
 {
   int j,tcases;
-  long int i=1,sum=0,n;
+  int i=1,sum=0,n,minmultiple;
   scanf("%d",&tcases);
   for(j=0;j<tcases;j++)
   {
-    scanf("%ld",&n);
+    scanf("%d",&n);
     i=1;
     sum=0;
-    // calc the sum here
+    //calc the sum here
     while(i<=n)
     {
-      sum+=(i*n)/gcd(i,n);
+      minmultiple=i;
+      while(minmultiple%i==0 && minmultiple%n==0)
+      {
+        minmultiple++;
+      }
+      sum+=minmultiple;
       i++;
     }
-    printf("%ld\n",sum);
+    printf("%d\n",sum);
   }
-  return 0;
+  return 0; 
 }
-long gcd(long a,long b)// to find gcd of 2 nos using euclids
+
+
+
+/*int gcd(int a,int b)// to find gcd of 2 nos using euclids
 {
   if(b==0) return a;
   return gcd(b,a%b);
-}
+}*/
